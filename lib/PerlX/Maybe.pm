bc3dd440e0ee21092da0a3dd8bda019c8a60a3f6
Toby Inkster <mail@tobyinkster.co.uk>
2013-08-10 14:05:28 +0100

    bump version; changelog

--- lib/PerlX/Maybe.pm
+++ lib/PerlX/Maybe.pm
@@ -5,7 +5,7 @@ use strict;
 
 BEGIN {
 	$PerlX::Maybe::AUTHORITY = 'cpan:TOBYINK';
-	$PerlX::Maybe::VERSION   = '0.999_01';
+	$PerlX::Maybe::VERSION   = '0.999_02';
 	
 	require Exporter;
 	our @ISA         = qw/ Exporter /;
